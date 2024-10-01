32be6c1ec98df2367fa18144356ef8d4e925195f
Ricardo Souza <ricksouz96@gmail.com>
2024-10-01 20:52:23 -0300

    Shows the battery percentage at the display

--- ElonsToys.cs
+++ ElonsToys.cs
@@ -4,17 +4,15 @@ using System.Reflection.Metadata.Ecma335;
 class RemoteControlCar
 {
     private int meters = 0;
+    private int battery = 100;
     public static RemoteControlCar Buy() => new RemoteControlCar();
 
     public string DistanceDisplay() => $"Driven {meters} meters";
 
-    public string BatteryDisplay()
-    {
-        throw new NotImplementedException("Please implement the RemoteControlCar.BatteryDisplay() method");
-    }
+    public string BatteryDisplay() => $"Battery at {battery}%";
 
     public void Drive()
     {
-        throw new NotImplementedException("Please implement the RemoteControlCar.Drive() method");
+
     }
 }
