f2ad3cd2152a9f7b6ddf7c21d4bc116c59ee2a37
Ricardo Souza <ricksouz96@gmail.com>
2024-10-01 20:37:24 -0300

    Add project files.

---
+++ ElonsToys.cs
@@ -0,0 +1,21 @@
+using System;
+
+class RemoteControlCar
+{
+    public static RemoteControlCar Buy() => new RemoteControlCar();
+
+    public string DistanceDisplay()
+    {
+        throw new NotImplementedException("Please implement the RemoteControlCar.DistanceDisplay() method");
+    }
+
+    public string BatteryDisplay()
+    {
+        throw new NotImplementedException("Please implement the RemoteControlCar.BatteryDisplay() method");
+    }
+
+    public void Drive()
+    {
+        throw new NotImplementedException("Please implement the RemoteControlCar.Drive() method");
+    }
+}
